a3e3357cb5361c6ace3f690ebf7e23323b77886d
Iwo Plaza <iwoplaza@gmail.com>
2026-06-04 18:08:18 +0200

    Deploy to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite-plus";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: "/word-learning/",
   plugins: [
     {
       ...basicSsl(),
